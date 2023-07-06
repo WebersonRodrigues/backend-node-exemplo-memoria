ca4dda56f004da7b4cbda41406e0e97e32deb637
weberson Rodrigues <weberson.r.a@gmail.com>
2023-07-05 21:48:53 -0300

    :sparkles: Adicionando envi de autenticação para exemplo com os alunos

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-// const Produto = require('./src/model/Produto');
 const routes = require('./routes');
 
-const porta = process.env.PORTA;
+const porta = process.env.PORTA || 3000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 app.use(routes);
-
-// const servicoProduto = require('./src/service/servicoProduto');
 
 app.get('/', (req, res) => {
     res.send("Api de controle de produtos está em funcionamento!");
